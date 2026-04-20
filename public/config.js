aa686066bc786674f2ea0b281bbc189730ecd82f
ivanlipay-eng <ivanlipay-eng@users.noreply.github.com>
2026-04-20 13:50:43 -0500

    chore: refresh public backend url

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.CNC_TECH_CONFIG = {
-  apiBaseUrl: "https://warned-patent-champions-ears.trycloudflare.com",
+  apiBaseUrl: "https://mil-despite-also-unfortunately.trycloudflare.com",
   brandLogoUrl: "",
 };
