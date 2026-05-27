d522c02bceea305daad4326ecb6f3a3dc3994b6a
ivanlipay-eng <ivanlipay-eng@users.noreply.github.com>
2026-05-26 22:16:34 -0500

    Update public backend tunnel

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.CNC_TECH_CONFIG = {
-  apiBaseUrl: "https://joseph-towns-folding-aircraft.trycloudflare.com",
+  apiBaseUrl: "https://raised-tire-individually-powder.trycloudflare.com",
   brandLogoUrl: "",
 };
